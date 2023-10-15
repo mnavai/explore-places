7fb8fc3fb4f2fa72839a4d232d5438e97443d9d3
Maryam Navaei <maryam.navaei@gmail.com>
2023-10-15 11:37:39 -0400

    implementing home components

--- App/Components/Home/Header.js
+++ App/Components/Home/Header.js
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 const Header = () => {
   return (
     <View>
-      
+      <Image source={require} />
     </View>
   )
 }
