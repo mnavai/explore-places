de4ed849de950b403b81d6b58068082a6f7de993
Maryam Navaei <maryam.navaei@gmail.com>
2023-10-14 21:22:57 -0400

    adding attributes to components

--- App/Navigations/TabNavigation.js
+++ App/Navigations/TabNavigation.js
@@ -1,22 +1,33 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text } from "react-native";
+import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../Screen/Home";
 import Fav from "../Screen/Fav";
 import Search from "../Screen/Search";
-import Profile from '../Screen/Profile';
+import Profile from "../Screen/Profile";
 
 const TabNavigation = () => {
-  const Tab = createBottomTabNavigator();  
+  const Tab = createBottomTabNavigator();
   return (
-    <Tab.Navigator screenOptions={{
-        headerShown:false
-    }}>
-      <Tab.Screen name="Home" component={Home} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+        }}
+      />
       <Tab.Screen name="Search" component={Search} />
-      <Tab.Screen name="Profile" component={Profile} />
       <Tab.Screen name="Fav" component={Fav} />
+      <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   );
-}
+};
 export default TabNavigation;
