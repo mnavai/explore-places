a9f46428673d3419502021a00cb28cab73efc568
Maryam Navaei <maryam.navaei@gmail.com>
2023-10-15 12:11:21 -0400

    implementing home components

--- App/Components/Home/Header.js
+++ App/Components/Home/Header.js
@@ -1,12 +1,20 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native';
+import { StyleSheet } from 'react-native';
+import React from 'react';
 
 const Header = () => {
   return (
     <View>
-      <Image source={require} />
+      <Image source={require('./../../../assets/logo.png')} style={styles.logo} />
     </View>
   )
 }
 
 export default Header;
+
+const styles = StyleSheet.create({
+    logo: {
+        width: 50,
+        height: 50
+    }
+})
