3a3bd31459877c04918ac1386f8a6a43149a4a2b
Maryam Navaei <maryam.navaei@gmail.com>
2023-10-14 21:17:10 -0400

    added Tab Navigation

--- App/Navigations/TabNavigation.js
+++ App/Navigations/TabNavigation.js
@@ -3,12 +3,18 @@ import React from 'react';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../Screen/Home";
 import Fav from "../Screen/Fav";
+import Search from "../Screen/Search";
+import Profile from '../Screen/Profile';
 
 const TabNavigation = () => {
   const Tab = createBottomTabNavigator();  
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={{
+        headerShown:false
+    }}>
       <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Profile" component={Profile} />
       <Tab.Screen name="Fav" component={Fav} />
     </Tab.Navigator>
   );
