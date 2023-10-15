6a2d528da4cd60d0e7fa297a7f7689c4a0a06457
Maryam Navaei <maryam.navaei@gmail.com>
2023-10-14 20:53:56 -0400

    adding components

--- App/Screen/Fav.js
+++ App/Screen/Fav.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Home() {
+export default function Fav() {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Fav</Text>
     </View>
   )
 }
--- App/Screen/Search.js
+++ App/Screen/Search.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Home() {
+export default function Search() {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Search</Text>
     </View>
   )
 }
