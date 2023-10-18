980ab54bc74324e192348baed68ff2806aeab3a1
Maryam Navaei <maryam.navaei@gmail.com>
2023-10-18 19:00:00 -0400

    creating fetching nearby places

--- App/Screen/Home.js
+++ App/Screen/Home.js
@@ -1,10 +1,21 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from "../Components/Home/Header";
 import GoogleMapView from '../Components/Home/GoogleMapView';
 import CategoryList from '../Components/Home/CategoryList';
+import GlobalApi from '../Services/GlobalApi';
 
 const Home = () => {
+
+  useEffect(()=>{
+    GetNearbySearchPlace();
+  },[])
+
+  const GetNearbySearchPlace = () => {
+    GlobalApi.nearByPlace().then(resp => {
+      console.log(resp.data.results)
+    })
+  }
   return (
     <View style={{padding:40}}>
       <Header />
