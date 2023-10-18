c4f62e4ce23d821ec9e54db0f7b86b0b50df38db
Maryam Navaei <maryam.navaei@gmail.com>
2023-10-17 22:16:49 -0400

    adding google map

--- App/Components/Home/GoogleMapView.js
+++ App/Components/Home/GoogleMapView.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
+import React from "react";
+import MapView from "react-native-maps";
 
-const GoogleMapView = () => {
+export default function GoogleMapView() {
   return (
-    <Text>GoogleMapView</Text>
-  )
+    <View style={{ marginTop: 20 }}>
+      <MapView
+        style={{
+          width:Dimensions.get("screen").width * 0.82,
+          height:Dimensions.get("screen").height * 0.23,
+        }}
+      ></MapView>
+    </View>
+  );
 }
-export default GoogleMapView;
