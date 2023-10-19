65dcba386390ed8ca0374482bea7b76dcaa501a3
Maryam Navaei <maryam.navaei@gmail.com>
2023-10-18 20:59:40 -0400

    displaying fetched data on UI

--- App/Components/Home/PlaceItem.js
+++ App/Components/Home/PlaceItem.js
@@ -1,16 +1,22 @@
 import { View, Text, Image } from 'react-native'
+import { AntDesign } from "@expo/vector-icons"; 
 import React from 'react'
+
 
 const PlaceItem = ({ place }) => {
   return (
-    <View>
+    <View style={{ display:'flex', flexDirection:'row', flex:1}}>
       <Image
         source={require("./../../../assets/placeholder.jpg")}
         style={{ width: 120, height: 120 }}
       />
       <View>
-        <Text>{place.name}</Text>
-        <Text>{place.vicinity}</Text>
+        <Text style={{fontSize:16, fontFamily:'Raleway-Semibold'}}>{place.name}</Text>
+        <Text style={{fontSize:16}}>{place.vicinity}</Text>
+      </View>
+      <View>
+        <AntDesign name="star" size={20} color="black" />
+        <Text>{place.rating}</Text>
       </View>
     </View>
   );
