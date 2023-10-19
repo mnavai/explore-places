72bbf8bd4c4b2a550741d15b3692147d6a93e446
Maryam Navaei <maryam.navaei@gmail.com>
2023-10-18 22:44:27 -0400

    styling the home page

--- App/Components/Home/PlaceItem.js
+++ App/Components/Home/PlaceItem.js
@@ -12,29 +12,44 @@ const PlaceItem = ({ place }) => {
         flex: 1,
         width: "100%",
         alignItems: "center",
-        gap:15,
-        marginBottom:20
+        gap: 15,
+        marginBottom: 20,
       }}
     >
       <Image
-        source={require("./../../../assets/placeholder.jpg")}
-        style={{ width: 100, height: 100, borderRadius:15 }}
+        source={{
+          uri:
+            "https://maps.googleapis.com/maps/api/place/photo" +
+            "?maxwidth=400" +
+            "&photo_reference=" +
+            place?.photos[0]?.photo_reference +
+            "&key=AIzaSyBVpVXdCtGdyQA1ncC4LN1jvpZThzT9vNA",
+        }}
+        style={{ width: 100, height: 100, borderRadius: 15 }}
       />
-      <View style={{flex:1}}>
-        <Text 
-            numberOfLines={2}
-            style={{ fontSize: 14, fontFamily: "Raleway-Semibold", marginBottom:5  }}>
-            {place.name}
+      <View style={{ flex: 1 }}>
+        <Text
+          numberOfLines={2}
+          style={{
+            fontSize: 14,
+            fontFamily: "Raleway-Semibold",
+            marginBottom: 5,
+          }}
+        >
+          {place.name}
         </Text>
         <Text
-            numberOfLines={2} 
-            style={{ fontSize: 12, marginBottom:10, color:Colors.DARKGRAY }}>{place.vicinity}</Text>
+          numberOfLines={2}
+          style={{ fontSize: 12, marginBottom: 10, color: Colors.DARKGRAY }}
+        >
+          {place.vicinity}
+        </Text>
         <View
           style={{
             display: "flex",
             flexDirection: "row",
             alignItems: "center",
-            gap:5
+            gap: 5,
           }}
         >
           <AntDesign name="star" size={20} color={Colors.YELLOW} />
