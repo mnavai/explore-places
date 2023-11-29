3eaf68fdc22e1f6cfe941d350555f71481d8b936
MaryaM <mnavai@MaryaMs-MBP.lan>
2023-11-28 23:49:04 -0500

    modal added

--- App/Navigations/HomeNavigation.js
+++ App/Navigations/HomeNavigation.js
@@ -1,14 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
+import { TransitionPresets, createStackNavigator } from '@react-navigation/stack'
 import Home from '../Screen/Home'
 import PlaceDetail from '../Components/PlaceDetail/PlaceDetail'
 const HomeNavigation = () => {
+  const isAndroid = true
   const Stack=createStackNavigator()  
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{gestureEnabled:true, headerShown:false,...(isAndroid&&TransitionPresets.ModalPresentationIOS)}}>
         <Stack.Screen name='home' component={Home} />
-        <Stack.Screen name='place-detail' component={PlaceDetail} />
+        <Stack.Screen name='place-detail' component={PlaceDetail} screenOptions={{presentation: 'modal'}} />
     </Stack.Navigator>
   )
 }
