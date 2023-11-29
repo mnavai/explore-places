a304c73a0f955abbdda302a3923e0db75f726900
MaryaM <mnavai@MaryaMs-MBP.lan>
2023-11-28 20:58:49 -0500

    real time location functionality added

--- App/Screen/Home.js
+++ App/Screen/Home.js
@@ -1,30 +1,31 @@
 import { ScrollView } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Header from "../Components/Home/Header";
 import GoogleMapView from '../Components/Home/GoogleMapView';
 import CategoryList from '../Components/Home/CategoryList';
 import GlobalApi from '../Services/GlobalApi';
 import PlaceList from '../Components/Home/PlaceList';
+import { UserLocationContext } from '../Context/UserLocationContext';
 
 const Home = () => {
   const [placeList, setPlaceList] = useState([]);
+  const {location, setLocation} = useContext(UserLocationContext)
 
   useEffect(()=>{
-    GetNearbySearchPlace();
+    GetNearbySearchPlace('restaurant');
   },[])
 
-  const GetNearbySearchPlace = () => {
-    GlobalApi.nearByPlace().then(resp => {
+  const GetNearbySearchPlace = (value) => {
+    GlobalApi.nearByPlace(location.coords.latitude,location.coords.longitude, value).then(resp => {
       console.log("res",resp.data.results)
       setPlaceList(resp.data.results)
-      console.log("res",resp.data.results)
     })
   }
   return (
     <ScrollView style={{padding:40}}>
       <Header />
       <GoogleMapView />
-      <CategoryList />
+      <CategoryList setSelectedCategory={(value) => GetNearbySearchPlace(value)} />
       {placeList ? <PlaceList placeList={placeList} /> : null}
     </ScrollView>
   )
--- App/Services/GlobalApi.js
+++ App/Services/GlobalApi.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const BASE_URL = "https://maps.googleapis.com/maps/api/place";
 const API_KEY = "AIzaSyBVpVXdCtGdyQA1ncC4LN1jvpZThzT9vNA"; 
 
-const nearByPlace = () => {
+const nearByPlace = (lat,lng,type) => {
   return axios.get(
     BASE_URL +
-      "/nearbysearch/json?" +
-      "keyword=cruise&location=-33.8670522%2C151.1957362&radius=1500&type=restaurant" +
+      "/nearbysearch/json?"+
+      "&location="+lat+","+lng+"&radius=1500&type="+type+
       "&key=" +
       API_KEY
   );
